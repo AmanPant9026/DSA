53f1de7a0461bad2fd7344f89a3238826ab4c19d
AMAN <pantaman98@gmail.com>
2024-06-17 14:04:37 +0530

    Added 3 new problems

---
+++ Arrays/3_Hard/07_merge_sorted_arrays_without_using_extra_space.cpp
@@ -0,0 +1,124 @@
+//Brute Force using extra space
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void merge(long long arr1[], long long arr2[], int n, int m) {
+
+//     //Declare a 3rd array and 2 pointers:
+//     long long arr3[n + m];
+//     int left = 0;
+//     int right = 0;
+
+//     int index = 0;
+
+//     //Insert the elements from the 2 arrays
+//     // into the 3rd array using left and right
+//     // pointers:
+
+//     while (left < n && right < m) {
+//         if (arr1[left] <= arr2[right]) {
+//             arr3[index] = arr1[left];
+//             left++, index++;
+//         }
+//         else {
+//             arr3[index] = arr2[right];
+//             right++, index++;
+//         }
+//     }
+
+//     // If right pointer reaches the end:
+//     while (left < n) {
+//         arr3[index++] = arr1[left++];
+//     }
+
+//     // If left pointer reaches the end:
+//     while (right < m) {
+//         arr3[index++] = arr2[right++];
+//     }
+
+//     // Fill back the elements from arr3[]
+//     // to arr1[] and arr2[]:
+//     for (int i = 0; i < n + m; i++) {
+//         if (i < n) arr1[i] = arr3[i];
+//         else arr2[i - n] = arr3[i];
+//     }
+// }
+
+// int main()
+// {
+//     long long arr1[] = {1, 4, 8, 10};
+//     long long arr2[] = {2, 3, 9};
+//     int n = 4, m = 3;
+//     merge(arr1, arr2, n, m);
+//     cout << "The merged arrays are: " << "\n";
+//     cout << "arr1[] = ";
+//     for (int i = 0; i < n; i++) {
+//         cout << arr1[i] << " ";
+//     }
+//     cout << "\narr2[] = ";
+//     for (int i = 0; i < m; i++) {
+//         cout << arr2[i] << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+// Note-
+// 1)Time Complexity: O(n+m) + O(n+m), where n and m are the sizes of the given arrays.
+// Reason: O(n+m) is for copying the elements from arr1[] and arr2[] to arr3[]. And another O(n+m) is for filling back the two given arrays from arr3[].
+// 2)Space Complexity: O(n+m) as we use an extra array of size n+m.
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Optimal Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(long long arr1[], long long arr2[], int n, int m) {
+
+    //Declare 2 pointers:
+    int left = n - 1;
+    int right = 0;
+
+    //Swap the elements until arr1[left] is
+    // smaller than arr2[right]:
+    while (left >= 0 && right < m) {
+        if (arr1[left] > arr2[right]) {
+            swap(arr1[left], arr2[right]);
+            left--, right++;
+        }
+        else {
+            break;
+        }
+    }
+
+    // Sort arr1[] and arr2[] individually:
+    sort(arr1, arr1 + n);
+    sort(arr2, arr2 + m);
+}
+
+int main()
+{
+    long long arr1[] = {1, 4, 8, 10};
+    long long arr2[] = {2, 3, 9};
+    int n = 4, m = 3;
+    merge(arr1, arr2, n, m);
+    cout << "The merged arrays are: " << "\n";
+    cout << "arr1[] = ";
+    for (int i = 0; i < n; i++) {
+        cout << arr1[i] << " ";
+    }
+    cout << "\narr2[] = ";
+    for (int i = 0; i < m; i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
+// Note-
+// 1)Time Complexity: O(min(n, m)) + O(n*logn) + O(m*logm), where n and m are the sizes of the given arrays.
+// Reason: O(min(n, m)) is for swapping the array elements. And O(n*logn) and O(m*logm) are for sorting the two arrays.
+// 2)Space Complexity: O(1) as we are not using any extra space.
