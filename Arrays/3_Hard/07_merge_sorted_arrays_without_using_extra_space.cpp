ffdfb46a51a8fc1cffa75fd6b6280e2e4bb48fb1
AMAN <pantaman98@gmail.com>
2024-06-17 14:22:27 +0530

    Added one extra solution

--- Arrays/3_Hard/07_merge_sorted_arrays_without_using_extra_space.cpp
+++ Arrays/3_Hard/07_merge_sorted_arrays_without_using_extra_space.cpp
@@ -73,30 +73,103 @@
 
 
 //Optimal Approach
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void merge(long long arr1[], long long arr2[], int n, int m) {
+
+//     //Declare 2 pointers:
+//     int left = n - 1;
+//     int right = 0;
+
+//     //Swap the elements until arr1[left] is
+//     // smaller than arr2[right]:
+//     while (left >= 0 && right < m) {
+//         if (arr1[left] > arr2[right]) {
+//             swap(arr1[left], arr2[right]);
+//             left--, right++;
+//         }
+//         else {
+//             break;
+//         }
+//     }
+
+//     // Sort arr1[] and arr2[] individually:
+//     sort(arr1, arr1 + n);
+//     sort(arr2, arr2 + m);
+// }
+
+// int main()
+// {
+//     long long arr1[] = {1, 4, 8, 10};
+//     long long arr2[] = {2, 3, 9};
+//     int n = 4, m = 3;
+//     merge(arr1, arr2, n, m);
+//     cout << "The merged arrays are: " << "\n";
+//     cout << "arr1[] = ";
+//     for (int i = 0; i < n; i++) {
+//         cout << arr1[i] << " ";
+//     }
+//     cout << "\narr2[] = ";
+//     for (int i = 0; i < m; i++) {
+//         cout << arr2[i] << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+// Note-
+// 1)Time Complexity: O(min(n, m)) + O(n*logn) + O(m*logm), where n and m are the sizes of the given arrays.
+// Reason: O(min(n, m)) is for swapping the array elements. And O(n*logn) and O(m*logm) are for sorting the two arrays.
+// 2)Space Complexity: O(1) as we are not using any extra space.
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Optimal Approach 2
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(long long arr1[], long long arr2[], int n, int m) {
-
-    //Declare 2 pointers:
-    int left = n - 1;
-    int right = 0;
-
-    //Swap the elements until arr1[left] is
-    // smaller than arr2[right]:
-    while (left >= 0 && right < m) {
-        if (arr1[left] > arr2[right]) {
-            swap(arr1[left], arr2[right]);
-            left--, right++;
-        }
-        else {
-            break;
-        }
+void swapIfGreater(long long arr1[], long long arr2[], int ind1, int ind2) {
+    if (arr1[ind1] > arr2[ind2]) {
+        swap(arr1[ind1], arr2[ind2]);
     }
+}
 
-    // Sort arr1[] and arr2[] individually:
-    sort(arr1, arr1 + n);
-    sort(arr2, arr2 + m);
+void merge(long long arr1[], long long arr2[], int n, int m) {
+    // len of the imaginary single array:
+    int len = n + m;
+
+    // Initial gap:
+    int gap = (len / 2) + (len % 2);
+
+    while (gap > 0) {
+        // Place 2 pointers:
+        int left = 0;
+        int right = left + gap;
+        while (right < len) {
+            // case 1: left in arr1[]
+            //and right in arr2[]:
+            if (left < n && right >= n) {
+                swapIfGreater(arr1, arr2, left, right - n);
+            }
+            // case 2: both pointers in arr2[]:
+            else if (left >= n) {
+                swapIfGreater(arr2, arr2, left - n, right - n);
+            }
+            // case 3: both pointers in arr1[]:
+            else {
+                swapIfGreater(arr1, arr1, left, right);
+            }
+            left++, right++;
+        }
+        // break if iteration gap=1 is completed:
+        if (gap == 1) break;
+
+        // Otherwise, calculate new gap:
+        gap = (gap / 2) + (gap % 2);
+    }
 }
 
 int main()
@@ -117,8 +190,3 @@ int main()
     cout << endl;
     return 0;
 }
-
-// Note-
-// 1)Time Complexity: O(min(n, m)) + O(n*logn) + O(m*logm), where n and m are the sizes of the given arrays.
-// Reason: O(min(n, m)) is for swapping the array elements. And O(n*logn) and O(m*logm) are for sorting the two arrays.
-// 2)Space Complexity: O(1) as we are not using any extra space.
